d30cfddb5be4d990ee318c5d9c67aade3fd2697a
thayron <thayronf3@gmail.com>
2021-02-17 23:39:39 -0300

    Foi criado uma interface com os dados que vamos precisar url e description para que seja possivel buscar as fotos da api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+interface Iphotos {
+  url: string;
+  description: string;
+}
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  photos = [];
-  constructor(htpp: HttpClient) {
-    console.log(htpp);
 
+export class AppComponent {
+
+  photos: Iphotos[] = [];
+
+  constructor(http:HttpClient) {
+
+    http
+    .get<Iphotos[]>('http://localhost:3000/flavio/photos')
+    .subscribe(photos => this.photos = photos);
   }
 }
